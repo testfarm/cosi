1a2ba4768e39a0ee82183c922e9799b4f6004827
Sylvain Giroudon <sylvain.giroudon@testfarm.org>
2016-04-13 17:12:54 +0200

    BUG: Do not override VERSION number is specified manually

--- pkg.mk
+++ pkg.mk
@@ -17,7 +17,7 @@
 
 DEBARCH = $(shell dpkg-architecture -qDEB_HOST_ARCH)
 
-VERSION := $(shell git describe --long --always --dirty 2>/dev/null || cat VERSION 2>/dev/null)
+VERSION ?= $(shell git describe --long --always --dirty 2>/dev/null || cat VERSION 2>/dev/null)
 SHORT_VERSION := $(shell echo $(VERSION) | sed -e 's/-dirty$$//' -e 's/-[a-zA-Z0-9]\+$$//')
 PKGVERSION ?= $(shell echo $(SHORT_VERSION:v%=%) | cut -d- -f1)
 PKGRELEASE ?= $(shell echo $(SHORT_VERSION:v%=%) | cut -d- -f2)
